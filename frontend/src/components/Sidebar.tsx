688b377471b3ab244c7a552e8b5bec7c9504794c
Saurabh kori <156657737+thesaurabhkori@users.noreply.github.com>
2024-09-17 17:27:58 +0530

    Updated Sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,42 +1,45 @@
-import React, {  } from 'react';
-import { IconHome, IconBell, IconUser, IconSearch, IconMessageCircle, IconBookmark, IconUsers } from '@tabler/icons-react';
+import React from 'react';
+import { IconHome, IconBell, IconUser, IconSearch, IconMessageCircle, IconBookmark, IconUsers, IconSettings2 } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
 
 const Sidebar: React.FC = () => {
- 
   return (
-    <div className="fixed flex flex-col items-center bg-gray p-6 h-full shadow-lg mr-32">
-      <div className="mb-8">
-        <h1 className="text-3xl font-bold">Connectify</h1>
+    <div className="fixed top-0 left-0 w-full md:w-1/4 lg:w-1/5 h-full bg-blue-950 p-4 md:p-6 flex flex-col items-center md:items-start shadow-lg">
+      <div className="mb-8 hidden md:block">
+        <h1 className="text-3xl font-bold text-white">Connectify</h1>
       </div>
-      <nav className="space-y-6 mb-8 w-full">
-        <Link to="/" className="flex items-center space-x-2 hover:bg-gray-100 p-2 rounded">
+      <nav className="space-y-0 w-full">
+        <Link to="/" className="flex items-center justify-center md:justify-start space-x-2 text-white hover:bg-gray-700 p-4 rounded border-t border-gray-700">
           <IconHome className="w-6 h-6" />
-          <span>Home</span>
+          <span className="hidden md:block">Home</span>
         </Link>
-        <Link to="#" className="flex items-center space-x-2 hover:bg-gray-100 p-2 rounded">
+        <Link to="#" className="flex items-center justify-center md:justify-start space-x-2 text-white hover:bg-gray-700 p-4 rounded border-t border-gray-700">
           <IconSearch className="w-6 h-6" />
-          <span>Explore</span>
+          <span className="hidden md:block">Explore</span>
         </Link>
-        <Link to="/notifications" className="flex items-center space-x-2 hover:bg-gray-100 p-2 rounded">
+        <Link to="/notifications" className="flex items-center justify-center md:justify-start space-x-2 text-white hover:bg-gray-700 p-4 rounded border-t border-gray-700">
           <IconBell className="w-6 h-6" />
-          <span>Notification</span>
+          <span className="hidden md:block">Notifications</span>
         </Link>
-        <Link to="#" className="flex items-center space-x-2 hover:bg-gray-100 p-2 rounded">
+        <Link to="#" className="flex items-center justify-center md:justify-start space-x-2 text-white hover:bg-gray-700 p-4 rounded border-t border-gray-700">
           <IconMessageCircle className="w-6 h-6" />
-          <span>Messages</span>
+          <span className="hidden md:block">Messages</span>
         </Link>
-        <Link to="#" className="flex items-center space-x-2 hover:bg-gray-100 p-2 rounded">
+        <Link to="#" className="flex items-center justify-center md:justify-start space-x-2 text-white hover:bg-gray-700 p-4 rounded border-t border-gray-700">
           <IconBookmark className="w-6 h-6" />
-          <span>Bookmarks</span>
+          <span className="hidden md:block">Bookmarks</span>
         </Link>
-        <Link to="/group" className="flex items-center space-x-2 hover:bg-gray-100 p-2 rounded">
+        <Link to="/group" className="flex items-center justify-center md:justify-start space-x-2 text-white hover:bg-gray-700 p-4 rounded border-t border-gray-700">
           <IconUsers className="w-6 h-6" />
-          <span>GroupPage</span>
+          <span className="hidden md:block">GroupPage</span>
         </Link>
-        <Link to="/profile" className="flex items-center space-x-2 hover:bg-gray-100 p-2 rounded">
+        <Link to="/profile" className="flex items-center justify-center md:justify-start space-x-2 text-white hover:bg-gray-700 p-4 rounded border-t border-gray-700">
           <IconUser className="w-6 h-6" />
-          <span>Profile</span>
+          <span className="hidden md:block">Profile</span>
+        </Link>
+        <Link to="/setting" className="flex items-center justify-center md:justify-start space-x-2 text-white hover:bg-gray-700 p-4 rounded border-t border-gray-700">
+          <IconSettings2 className="w-6 h-6" />
+          <span className="hidden md:block">Settings</span>
         </Link>
       </nav>
     </div>
